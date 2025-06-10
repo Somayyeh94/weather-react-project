3f1805058a3a0d491b299495175cf329222681e2
Somayyeh094 <s.somayyeh.seyedi@gmail.com>
2025-06-10 12:01:07 +0330

    added footer to app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ function App() {
   return (
     <div className="App">
       <h1>Weather application</h1>
+      <footer>
+        This projedt was coded by Somayyeh Seyedi and it is open-sourced on{" "}
+        <a
+          href="https://github.com/Somayyeh94/weather-react-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          GitHub
+        </a>
+      </footer>
     </div>
   );
 }
