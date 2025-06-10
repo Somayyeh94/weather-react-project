15ca6154d3fd703404e2ce9da6a54c2d1e00552d
Somayyeh094 <s.somayyeh.seyedi@gmail.com>
2025-06-10 12:23:16 +0330

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 
 function App() {
   return (
     <div className="App">
       <h1>Weather application</h1>
+      <button className="btn btn-primary">search</button>
       <footer>
         This projedt was coded by Somayyeh Seyedi and it is open-sourced on{" "}
         <a
